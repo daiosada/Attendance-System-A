8512382b154cc492c42c81ec8e70fd5598ee155a
Dai Osada <dai.osada@gmail.com>
2020-09-17 12:59:46 +0000

    Delete unnecessary migration files

--- db/migrate/20200913141241_change_class_to_attendances.rb
+++
@@ -1,8 +0,0 @@
-class ChangeClassToAttendances < ActiveRecord::Migration[5.1]
-  def change
-    change_column :attendances, :started_at, :time
-    change_column :attendances, :finished_at, :time
-    change_column :attendances, :changed_started_at, :time
-    change_column :attendances, :changed_finished_at, :time
-  end
-end
--- db/migrate/20200914120231_change_columns_to_attendances.rb
+++
@@ -1,8 +0,0 @@
-class ChangeColumnsToAttendances < ActiveRecord::Migration[5.1]
-  def change
-    change_column :attendances, :started_at, :datetime
-    change_column :attendances, :finished_at, :datetime
-    change_column :attendances, :changed_started_at, :datetime
-    change_column :attendances, :changed_finished_at, :datetime
-  end
-end
